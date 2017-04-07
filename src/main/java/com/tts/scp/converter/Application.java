5debb1ae28c93bfa345b1583ab89ec35ced18d92
Mathias Maerker <mathias.maerker@tt-s.com>
2017-04-07 12:42:28 +0200

    Refactoring und Jackson tests

--- src/main/java/com/tts/scp/converter/Application.java
+++ src/main/java/com/tts/scp/converter/Application.java
@@ -2,11 +2,24 @@ package com.tts.scp.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-public class Application {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);		
+@Profile({ "production", "dev" })
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.addInitializers(new EnvironmentContextInitializer());
+
+		app.run(args);
+	}
+
 }
--- src/main/java/com/tts/scp/converter/ConverterController.java
+++ src/main/java/com/tts/scp/converter/ConverterController.java
@@ -4,58 +4,53 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.tts.scp.converter.config.HttpProviderConfig;
+
 
 @RestController
 public class ConverterController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ConverterController.class);
 
-
+	@Autowired @Lazy
+	private HttpProviderConfig client;
+	
 	@GetMapping(path = "/convertDoc", produces = MediaType.APPLICATION_PDF_VALUE)
-	public void convertedDocument(@RequestParam(defaultValue = "true") boolean printResume,
+	public void convertedDocument(
+			@RequestParam(defaultValue = "true") boolean printResume,
 			@RequestParam(defaultValue = "true") boolean printCoverLetter,
-			@RequestParam(defaultValue = "true") boolean printAttachments, @RequestParam String applicationId, HttpServletResponse response) throws IOException, DocumentException {
-				
-		Document doc = new Document();
-		PdfWriter.getInstance(doc, response.getOutputStream());
-		doc.open();
-		Paragraph paragraph = new Paragraph();
-		
-		paragraph.add(new Paragraph("Hallo Welt"));
-		paragraph.add(new Paragraph(" "));
-		paragraph.add(new Paragraph("Hallo Welt 2"));
-		paragraph.add(new Paragraph(" "));
-		paragraph.add(new Paragraph("Hallo Welt 3"));
-		doc.add(paragraph);
-		doc.close();
-				
-//		PDDocument doc = new PDDocument();
-//		PDPage page = new PDPage();
-//		doc.addPage(page);
-//		   final PDFont courierBoldFont = PDType1Font.TIMES_ROMAN;  
-//		   final int fontSize = 12; 
-//		final PDPageContentStream contentStream = new PDPageContentStream(doc, page);  
-//	      contentStream.beginText();  
-//	      contentStream.setFont(courierBoldFont, fontSize);  
-//	      contentStream.setLeading(2.5);
-//	      contentStream.newLineAtOffset(150, 750);  
-//	      contentStream.showText("Hello PDFBox");
-//	      contentStream.newLine();
-//	      contentStream.showText("Hello PDFBox2");  
-//	      contentStream.endText();  
-//	      contentStream.close();  // Stream must be closed before saving document.  
-//	      doc.save(response.getOutputStream());
-//		  doc.close();
-		
+			@RequestParam(defaultValue = "true") boolean printAttachments, 
+			@RequestParam String applicationId,
+			HttpServletResponse response) throws IOException, DocumentException {
 
+		
+		HttpResponse httpResponse = client.getClient();
+		logger.info("HTTP RESPONSE STATUS IS: " + httpResponse.getStatusLine());
+		
+		
+//		Document doc = new Document();
+//		PdfWriter.getInstance(doc, response.getOutputStream());
+//		doc.open();
+//		Paragraph paragraph = new Paragraph();
+//
+//		paragraph.add(new Paragraph("Hallo Welt"));
+//		paragraph.add(new Paragraph(" "));
+//		paragraph.add(new Paragraph("Hallo Welt 2"));
+//		paragraph.add(new Paragraph(" "));
+//		paragraph.add(new Paragraph("Hallo Welt 3"));
+//		doc.add(paragraph);
+//		doc.close();
 
 	}
 }
